9e0455a8ac8016e922f70ffd8c91c1fa02642753
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-02 17:06:59 +0900

    feat: siginin 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import Todo from "./pages/Todo";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default App;
+}
