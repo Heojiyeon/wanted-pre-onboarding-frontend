e9f89a6c73701dff5ece9caa3b2a5d8294d14f31
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-02 16:58:31 +0900

    feat: 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div>React App</div>;
-}
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Signup from "./pages/Signup";
+import Todo from "./pages/Todo";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
