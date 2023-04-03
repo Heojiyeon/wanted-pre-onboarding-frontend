342c700dc825713ce6ee026c18ce9fa1c62be530
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-03 17:07:49 +0900

    feat: signin, signup form 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Link to="/signup">회원가입</Link>
+      <Link to="/signin">로그인</Link>
+    </div>
+  );
 }
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,3 +1,13 @@
 export default function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <form>
+        <label id="email-input">email</label>
+        <input data-testid="email-input" type="email" />
+        <label id="email-input">password</label>
+        <input data-testid="password-input" type="password" />
+        <button data-testid="signin-button">Login</button>
+      </form>
+    </div>
+  );
 }
