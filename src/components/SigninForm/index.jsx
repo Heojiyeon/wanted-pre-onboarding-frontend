e7cb38e57837cfd19ec46d11c50e41e46ac0f4b2
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-06 17:29:15 +0900

    feat: signin api 연동

--- src/components/SigninForm/index.jsx
+++ src/components/SigninForm/index.jsx
@@ -1,6 +1,27 @@
+import { useNavigate } from "react-router";
+import { baseRequest } from "../../apis/core";
+
 export default function SigninForm() {
+  const navigate = useNavigate();
+
+  const handleForm = async e => {
+    e.preventDefault();
+    try {
+      const response = await baseRequest.post("/auth/signin", {
+        email: e.target[0].value,
+        password: e.target[1].value,
+      });
+      if (response.status === 200) {
+        window.localStorage.setItem("access_token", response.data.access_token);
+        navigate("/todo");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={e => handleForm(e)}>
       <label id="email-input">email</label>
       <input data-testid="email-input" type="email" />
       <label id="email-input">password</label>
