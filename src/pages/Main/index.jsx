c45dd1fa7dd0725bc8298195fe8dda1cc4ecf4bd
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-11 16:36:03 +0900

    feat: 토큰 유무에 따른 라우팅 처리 (main)

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getAccessToken } from "../../utils/handleAccessToken";
 
 export default function Main() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getAccessToken("access_token")) {
+      navigate("/todo");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Link to="/signup">회원가입</Link>
