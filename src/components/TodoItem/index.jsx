b7a27fa0da5f8eae8d87f35729f6c963e490da71
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-19 01:32:54 +0900

    feat: updateTodo (수정) 기능 구현 및 api 연동

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
 import { baseRequest } from "../../apis/core";
 
-export default function TodoItem({
-  currTodo,
-  accessToken,
-  currCompleted,
-  getTodo,
-}) {
-  const [completed, setCompleted] = useState(currCompleted);
+export default function TodoItem({ currTodo, accessToken, getTodo }) {
+  const [completed, setCompleted] = useState(currTodo.isCompleted);
+  const [updatedTodo, setUpdatedTodo] = useState(currTodo.todo);
+  const [edited, setEdited] = useState(false);
 
   const updateTodo = async id => {
-    setCompleted(!completed);
     try {
-      await baseRequest.put(
-        `/todos/${id}`,
-        {
-          todo: currTodo.todo,
-          isCompleted: !completed,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
+      await baseRequest
+        .put(
+          `/todos/${id}`,
+          {
+            todo: updatedTodo,
+            isCompleted: !completed,
           },
-        }
-      );
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        )
+        .then(() => {
+          setEdited(false);
+        });
     } catch (error) {
       console.error(error);
     }
@@ -47,27 +47,44 @@ export default function TodoItem({
 
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          onChange={() => {
-            updateTodo(currTodo.id);
-          }}
-          checked={completed}
-        />
-        <span>{currTodo.todo}</span>
-      </label>
-      <button
-        data-testid="modify-button"
-        // onClick={() => updateTodo(currTodo.id)}
-      >
-        수정
-      </button>
-      <button
-        data-testid="delete-button"
-        onClick={() => deleteTodo(currTodo.id)}>
-        삭제
-      </button>
+      {!edited ? (
+        <>
+          <label>
+            <input
+              type="checkbox"
+              onChange={() => {
+                setCompleted(!completed);
+                updateTodo(currTodo.id);
+              }}
+              checked={completed}
+            />
+            <span>{updatedTodo}</span>
+          </label>
+          <button data-testid="modify-button" onClick={() => setEdited(true)}>
+            수정
+          </button>
+          <button
+            data-testid="delete-button"
+            onClick={() => deleteTodo(currTodo.id)}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            data-testid="modify-input"
+            onChange={e => setUpdatedTodo(e.target.value)}
+          />
+          <button
+            data-testid="submit-button"
+            onClick={() => updateTodo(currTodo.id)}>
+            제출
+          </button>
+          <button data-testid="cancel-button" onClick={() => setEdited(false)}>
+            취소
+          </button>
+        </>
+      )}
     </li>
   );
 }
