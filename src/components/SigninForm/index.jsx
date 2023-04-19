5521cd53f0871616e1cd37216f40c605123aa434
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-04-19 12:20:50 +0900

    feat: siginin 유효성 검사 추가

--- src/components/SigninForm/index.jsx
+++ src/components/SigninForm/index.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import { signInAuth } from "../../apis/auth";
 import { setAccessToken } from "../../utils/handleAccessToken";
+import { isValidEmail, isValidPassword } from "../../utils/validate/signup";
 import { StyledInput, StyledSignupButton, StyledSignupForm } from "./style";
+
 export default function SigninForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
   const handleForm = async e => {
     e.preventDefault();
 
-    const response = await signInAuth(e.target[0].value, e.target[1].value);
+    const response = await signInAuth(email, password);
 
     if (response.status === 200) {
       setAccessToken("access_token", response.data.access_token);
@@ -19,10 +25,20 @@ export default function SigninForm() {
   return (
     <StyledSignupForm onSubmit={e => handleForm(e)}>
       <label id="email-input">email</label>
-      <StyledInput data-testid="email-input" type="email" />
+      <StyledInput
+        data-testid="email-input"
+        type="email"
+        onChange={e => setEmail(e.target.value)}
+      />
       <label id="password-input">password</label>
-      <StyledInput data-testid="password-input" type="password" />
-      <StyledSignupButton data-testid="signin-button">
+      <StyledInput
+        data-testid="password-input"
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+      />
+      <StyledSignupButton
+        data-testid="signin-button"
+        disabled={!(isValidPassword(password) && isValidEmail(email))}>
         Sign in
       </StyledSignupButton>
     </StyledSignupForm>
